250d18c3aeb4796584e69924ff3b28f6efcaf77a
thedance546 <87199693+thedance546@users.noreply.github.com>
2024-04-11 14:03:43 +0900

    Title: a 부터 b 까지, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240411/a 부터 b 까지/a-to-b.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int i = a; i <= b;) {
+            if(i%2==1){
+                System.out.print(i+" ");
+                i*=2;
+                
+            } else{
+                System.out.print(i+" ");
+                i+=3;
+                
+            }
+            
+        }
+    }
+}
