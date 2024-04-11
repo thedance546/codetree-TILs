309671cd45e202e050f388612047cf32c253ed3d
thedance546 <87199693+thedance546@users.noreply.github.com>
2024-04-11 11:47:11 +0900

    Title: a/b 출력, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 240411/a/b 출력/a-divide-b.java
+++ 240411/a/b 출력/a-divide-b.java
@@ -1,12 +1,22 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        double result = (double)a/(double)b;
+        int d = ((a%b)*10);
+        int result=0;
+        int c=0;
+        
+        System.out.print(a/b+".");
+        for(int i=0; i<20; i++){
+            System.out.print(d/b);
+            c=d/b;
+            result+=0.1*c;
+            d=d%b*10;
+        }
 
-        System.out.printf("%.20f",result);
     }
 }
