0db44648f7d465a302382889679ea186fe68782e
thedance546 <87199693+thedance546@users.noreply.github.com>
2024-04-11 14:34:03 +0900

    Title: 369 게임, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240411/369 게임/369-game.java
+++ 240411/369 게임/369-game.java
@@ -11,7 +11,13 @@ public class Main {
                 System.out.print("0 ");
             } else if(i%10==3 || i%10==6 || i%10==9) {
                 System.out.print("0 ");
-            } else if(i%100==3 || i%100==6 || i%100==9) {
+            } else if(i/10==3 || i/10==6 || i/10==9) {
+                System.out.print("0 ");
+            } else if((i%100)%10==3 || (i%100)%10==6 || (i%100)%10==9) {
+                System.out.print("0 ");
+            }else if((i/10)%10==3 || (i/10)%10==6 || (i/10)%10==9) {
+                System.out.print("0 ");
+            }else if(i/100==3 || i/100==6 || i/100==9) {
                 System.out.print("0 ");
             } else {
                 System.out.print(i+" ");
