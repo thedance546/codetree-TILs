7c6a575bf0a001cc99c92f712a96d4a2b93d8b25
thedance546 <87199693+thedance546@users.noreply.github.com>
2024-05-08 22:06:51 +0900

    Title: 1부터의 합, Time: 126ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240508/1부터의 합/sum-from-the-one.java
+++ 240508/1부터의 합/sum-from-the-one.java
@@ -8,10 +8,11 @@ public class Main {
         int sum=0;
 
         for (int i = 1; i <= 100; i++) {
-            sum+=i;
+            
             if(sum>=n){
                 break;
             }
+            sum+=i;
         }
 
         System.out.println(sum);
