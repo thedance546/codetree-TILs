4460fa3129eb485e50ee94400d9537ec45bd88f0
thedance546 <87199693+thedance546@users.noreply.github.com>
2024-05-08 22:09:44 +0900

    Title: 1부터 곱셈하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240508/1부터 곱셈하기/multiple-from-one.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int prod=1;
+
+        for (int i = 1; i <= 10; i++) {
+            prod*=i
+            if(pro==n){
+                System.out.println(i);
+                break;
+            }
+            
+        }
+
+        
+    }
+}
