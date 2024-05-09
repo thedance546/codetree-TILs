d70902bf044f438103f04c7aa734bb602b05bc3b
thedance546 <87199693+thedance546@users.noreply.github.com>
2024-05-09 09:48:41 +0900

    Title: 몫 출력하기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240509/몫 출력하기/print-share.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        
+        for(int i=0; i<3; i++){
+            n=sc.nextInt();
+            if(n%2==1){
+                continue;
+            } else{
+               System.out.println(n/2);
+            }
+        }
+    }
+}
