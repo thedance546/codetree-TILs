b4adc977834988ea0f705a3c49697417a29d3b3d
thedance546 <87199693+thedance546@users.noreply.github.com>
2024-05-10 20:35:47 +0900

    Title: 정사각형 별표 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240510/정사각형 별표 출력하기/print-stars-in-square.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
