3c4af9d801228a0024d98b17e0366a1bfe15e9f7
thedance546 <87199693+thedance546@users.noreply.github.com>
2024-05-10 21:20:19 +0900

    Title: 직각 삼각형 출력, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240510/직각 삼각형 출력/Right-triangle-and-output.java
+++ 240510/직각 삼각형 출력/Right-triangle-and-output.java
@@ -4,12 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int k=1;
         
         for (int i = 0; i < n; i++) {
-            for(int j = 0; j <i+1; j++) {
+            for(int j = 0; j <i+k; j++) {
                 System.out.print("*");
             }
             System.out.println();
+            k++;
         }
     }
 }
