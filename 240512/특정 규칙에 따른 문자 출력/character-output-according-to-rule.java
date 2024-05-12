587ebf944061579d29b0064dbed2784b306906db
thedance546 <87199693+thedance546@users.noreply.github.com>
2024-05-12 21:55:28 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240512/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
+++ 240512/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
@@ -11,13 +11,13 @@ public class Main {
                 System.out.print(" ");
             }
             for (int j = 0; j < (i+1); j++) {
-                System.out.print("@");
+                System.out.print("@ ");
             }
             System.out.println();
         }
         for (int i = 0; i < n-1; i++) {
             for (int j = 0; j < (n-i-1); j++) {
-                System.out.print("@");
+                System.out.print("@ ");
             }
             System.out.println();
         }
