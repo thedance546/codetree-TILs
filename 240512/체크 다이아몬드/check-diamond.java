65441430af8aae222e02deff1ac7e59e3f58eee2
thedance546 <87199693+thedance546@users.noreply.github.com>
2024-05-12 22:25:49 +0900

    Title: 체크 다이아몬드, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240512/체크 다이아몬드/check-diamond.java
+++ 240512/체크 다이아몬드/check-diamond.java
@@ -7,7 +7,7 @@ public class Main {
         int n=sc.nextInt();
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j <n-i-1; j++) {
+            for (int j = 0; j <i+1; j++) {
                 System.out.print(" ");
             }
             for (int j = 0; j <i+1; j++) {
