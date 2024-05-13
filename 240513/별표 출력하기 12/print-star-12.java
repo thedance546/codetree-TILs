92a4d71f71bb193c8bfe4e1698a9ad504b1465a5
thedance546 <87199693+thedance546@users.noreply.github.com>
2024-05-13 14:39:06 +0900

    Title: 별표 출력하기 12, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 240513/별표 출력하기 12/print-star-12.java
+++ 240513/별표 출력하기 12/print-star-12.java
@@ -8,11 +8,16 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (j %2==0) {
-                    System.out.print("* ");
-                }
-                else {
-                    for(int k=0; k<j+1; k++){
+                if(j%2==0){
+                    if(i>0){
+                        System.out.print("  ");
+                    } else{
+                        System.out.print("* ");
+                    }
+                } else{
+                    if(i>j){
+                        System.out.print("  ");
+                    } else{
                         System.out.print("* ");
                     }
                 }
