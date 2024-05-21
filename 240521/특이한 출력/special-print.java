9c06e673ffdfcd32b5ac7bf469efee53dc20745d
thedance546 <87199693+thedance546@users.noreply.github.com>
2024-05-21 16:15:09 +0900

    Title: 특이한 출력, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240521/특이한 출력/special-print.java
+++ 240521/특이한 출력/special-print.java
@@ -5,16 +5,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int k=n;
+        int k=3;
 
         for (int i = 1; i < n+1; i++) {
             for (int j = 1; j < n+1; j++) {
                 if (j==k) {
                     System.out.println("(" + i + ", " + j + ")");
                     k--;
-                    if(k==0){
-                        k=n;
-                    }
                 }
                 else {
                     System.out.print("(" + i + ", " + j + ") ");
