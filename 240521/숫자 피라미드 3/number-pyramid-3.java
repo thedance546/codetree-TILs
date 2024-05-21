f03574061099f89f6fe134000015749b5204e324
thedance546 <87199693+thedance546@users.noreply.github.com>
2024-05-21 15:34:29 +0900

    Title: 숫자 피라미드 3, Time: 199ms, Memory: 11MB, Status: Passed - CodeTree

--- 240521/숫자 피라미드 3/number-pyramid-3.java
+++ 240521/숫자 피라미드 3/number-pyramid-3.java
@@ -7,7 +7,7 @@ public class Main {
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < i + 1; j++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print(i*(j+1)+" ");
             }
             System.out.println();
