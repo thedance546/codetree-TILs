564db8a17b8ddf83349e53b3d4856197615a21f1
thedance546 <87199693+thedance546@users.noreply.github.com>
2024-05-21 16:17:14 +0900

    Title: 특이한 출력, Time: 196ms, Memory: 10MB, Status: Passed - CodeTree

--- 240521/특이한 출력/special-print.java
+++ 240521/특이한 출력/special-print.java
@@ -9,12 +9,8 @@ public class Main {
 
         for (int i = 1; i < n+1; i++) {
             for (int j = 1; j < n+1; j++) {
-                if (j==k) {
+                if ((i+j)%4==0) {
                     System.out.println("(" + i + ", " + j + ")");
-                    k--;
-                    if(k==0){
-                        k=n;
-                    }
                 }
                 else {
                     System.out.print("(" + i + ", " + j + ") ");
