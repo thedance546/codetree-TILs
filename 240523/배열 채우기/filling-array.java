d8f30339d776a6882089ff74e85b21452e25f477
thedance546 <87199693+thedance546@users.noreply.github.com>
2024-05-23 14:50:28 +0900

    Title: 배열 채우기, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240523/배열 채우기/filling-array.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[10];
+        int cnt=0;
+
+        for (int i = 0; i < 10; i++) {
+            int n= sc.nextInt();
+            if(n!=0){
+                arr[i]=n; 
+                cnt++;
+            } else{
+                break;
+            }
+            
+        }
+        for(int j=cnt-1; j>=0; j--){
+            System.out.print(arr[j]+" ");
+        }
+
+    }
+}
